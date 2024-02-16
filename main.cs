e79f8e3b2852f2063a16f133865361ec694d446c
solumbris <158665326+solumbris@users.noreply.github.com>
2024-02-16 09:18:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    double denomination = 0.25;
+
+    // No Person is found for .01, .05, .25
+    string person_1 = "Jose Rizal";
+    string person_5 = "Emilio Aguinaldo";
+    string person_10 = "Andres Bonifacio, Apolinario Mabini";
+    string person_20 = "Manuel L. Quezon";
+    string person_50 = "Sergio Osmena";
+    string person_100 = "Manuel Roxas";
+    string person_200 = "Diosdado Macapagal";
+    string person_500 = "Benigno Sr. and Corazon Aquino";
+    string person_1000 = "Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda";
+
+  }
+}
